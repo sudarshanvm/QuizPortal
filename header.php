6dcc7617f567c05a046b8854f69dea86e1c3036a
rvce <you@example.com>
2020-03-06 11:29:11 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logo">Quiz Portal</span></div>
+<div class="col-md-2 col-md-offset-4">
+<a href="#" class="btn btn-primary logb" data-toggle="modal" data-target="#myModal"> <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b> Login </b> </span></a></div>
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span style="color:darkblue;font-size:12px;font-weight: bold">Login to your Account</span></h4>
+      </div>
+      <div class="modal-body">
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="icon" href="favicon.ico" type="image/icon" sizes="16x16">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title> Quiz Portal </title>
+   
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/> 
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+</head>
+
+<body >
+  <?php include 'header.php' ;?>
+
+</body>
+
+</html>
