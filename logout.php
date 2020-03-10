69bc208a860411ea77e1d871d186a2cb8bc2bfa1
Sudarshan VM <sudarshan.vm2000@gmail.com>
2020-03-11 00:39:20 +0530

    about.txt created

--- logout.php
+++ logout.php
@@ -3,6 +3,11 @@ session_start();
 if (isset($_SESSION['username'])) {
     session_destroy();
 }
-$ref = @$_GET['q'];
+// redirect to the homapage
+// $ref = @$_GET['q'];
+
+ // Delete the username cookie by setting their expirations to an hour ago (3600)
+setcookie('username', '', time() - 3600);
+
 header("location:index.php");
 ?>
